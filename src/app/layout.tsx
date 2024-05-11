866db624dc7aa5eae340e325e16ba57b69c1b9c3
Minh Nguyen <nguyenthanhminh112000@gmail.com>
2024-05-11 12:02:16 +0700

    refactor(DB-000005): update root layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,11 @@
 import '@shared/styles/global.css';
 import { inter } from '@shared/assets/font/inter';
 
-export default function RootLayout({
-  children,
-}: {
+type RootLayoutProps = {
   children: React.ReactNode;
-}) {
+};
+
+export default function RootLayout({ children }: RootLayoutProps) {
   return (
     <html lang="en">
       <body className={`${inter.className} antialiased`}>{children}</body>
