4cd9614913a68a560da1d440980e34f27600caf1
Minh Nguyen <nguyenthanhminh112000@gmail.com>
2024-05-12 12:36:55 +0700

    feat(DB-000012): streaming dashboard page

---
+++ src/features/dashboard/components/cards-skeleton.tsx
@@ -0,0 +1,12 @@
+import { CardSkeleton } from '@features/dashboard/components/card-skeleton';
+
+export function CardsSkeleton() {
+  return (
+    <>
+      <CardSkeleton />
+      <CardSkeleton />
+      <CardSkeleton />
+      <CardSkeleton />
+    </>
+  );
+}
---
+++ src/features/dashboard/components/dashboard-skeleton.tsx
@@ -0,0 +1,24 @@
+import { shimmer } from '@features/dashboard/styles/loading-animation';
+import { CardSkeleton } from '@features/dashboard/components/card-skeleton';
+import { RevenueChartSkeleton } from '@features/dashboard/components/revenue-skeleton';
+import { LatestInvoicesSkeleton } from '@features/dashboard/components/latest-invoices-skeleton';
+
+export function DashboardSkeleton() {
+  return (
+    <>
+      <div
+        className={`${shimmer} relative mb-4 h-8 w-36 overflow-hidden rounded-md bg-gray-100`}
+      />
+      <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
+        <CardSkeleton />
+        <CardSkeleton />
+        <CardSkeleton />
+        <CardSkeleton />
+      </div>
+      <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
+        <RevenueChartSkeleton />
+        <LatestInvoicesSkeleton />
+      </div>
+    </>
+  );
+}
